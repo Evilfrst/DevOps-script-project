04377c70e4ccafd8a10b729edea4ff21f25ac1fa
EvradDsoh <wqbw57ccsb@privaterelay.appleid.com>
2026-03-09 14:40:56 +0100

    Add files via upload

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,1 +1,3 @@
-
+Write-Host "Building project"
+if (!(Test-Path "../build")) { New-Item -ItemType Directory -Path "../build" }
+Compress-Archive -Path ../src/* -DestinationPath ../build/release.zip -Force
